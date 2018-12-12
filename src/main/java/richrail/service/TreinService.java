468f79a5498a6168474cb201d6733e8bc1d18d58
Jonathan <Jonathan.deGier@student.hu.nl>
2018-12-12 13:06:37 +0100

    get alle treinen

--- src/main/java/richrail/service/TreinService.java
+++ src/main/java/richrail/service/TreinService.java
@@ -3,10 +3,13 @@ package richrail.service;
 import richrail.domein.RollingComponent;
 import richrail.domein.Trein;
 
+import java.util.List;
+
 public interface TreinService {
 	
 	public boolean newTrein(String name);
 	public Trein getTrein(String name);
+	public List<Trein> getAlleTreinen();
 	public boolean removeTrain(String name);
 	
 	public boolean addComponentToTrain(String treinNaam, String componentNaam);
@@ -21,5 +24,6 @@ public interface TreinService {
 	public boolean createRollingComponent(String name, int gewicht,  String typeNaam);
 	public boolean createRollingComponent(String name, String typeNaam);
 	public RollingComponent getComponent(String componentNaam);
+	public List<RollingComponent> getAlleComponenten();
 	public boolean removeComponent(String componentNaam);
 }
