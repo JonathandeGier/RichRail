0f7de0bf1c3f4dc206bbeb270ed896baf82223e5
Jonathan <Jonathan.deGier@student.hu.nl>
2018-12-14 14:30:50 +0100

    TODO gedaan, kans op NullPointerExceptions verminderd en comments toegevoegd

--- src/main/java/richrail/service/TreinController.java
+++ src/main/java/richrail/service/TreinController.java
@@ -16,13 +16,16 @@ public class TreinController implements TreinService {
 	}
 	
 	public boolean newTrein(String name) {
-		Trein tr = new Trein(name);
-		return treinen.add(tr);
+		if(getTrein(name) == null) {																					// kijken of er al een trein is met dezelfde naam
+			Trein tr = new Trein(name);																					// nieuwe trein maken en toevoegen aan de list
+			return treinen.add(tr);
+		}
+		return false;
 	}
 	
 	public Trein getTrein(String name) {
 		for(Trein tr : treinen) {
-			if(tr.getName().equals(name)) {
+			if(tr.getName().equals(name)) {																				// door de treinen list loopen en kijken of de naam overeen komt
 				return tr;
 			}
 		}
@@ -30,29 +33,38 @@ public class TreinController implements TreinService {
 	}
 
 	public List<Trein> getAlleTreinen() {
-		return Collections.unmodifiableList(treinen);
+		return Collections.unmodifiableList(treinen);																	// list terug geven die je niet kan veranderen
 	}
 	
-	public boolean removeTrain(String name) {
+	public boolean removeTrain(String name) {																			// trein uit de list verwijderen
 		Trein tr = getTrein(name);
 		return treinen.remove(tr);
 	}
 	
 	public boolean addComponentToTrain(String treinNaam, String componentNaam) {
-		RollingComponent wagon = getComponent(componentNaam);
-		Trein tr = getTrein(treinNaam);
-		return losseComponenten.remove(wagon) && tr.addRollingComonent(wagon);
+		RollingComponent wagon = getComponent(componentNaam);															// wagon ophalen
+		Trein tr = getTrein(treinNaam);																					// trein ophalen
+		if(wagon != null && tr != null) {																				// testen of trein en wagon bestaan
+			return losseComponenten.remove(wagon) && tr.addRollingComonent(wagon);										// wagon uit losseComponenten halen en aan trein toevoegen
+		}
+		return false;
 	}
 	
 	public RollingComponent getComponentFromTrain(String treinNaam, String componentNaam) {
-		Trein tr = getTrein(treinNaam);
-		return tr.getRollingComponent(componentNaam);
+		Trein tr = getTrein(treinNaam);																					// trein ophalen
+		if(tr != null) {																								// testen of trein bestaat
+			return tr.getRollingComponent(componentNaam);																// component terug geven
+		}
+		return null;
 	}
 	
 	public boolean removeComponentFromTrain(String treinNaam, String componentNaam) {
-		Trein tr = getTrein(treinNaam);
-		RollingComponent component = getComponentFromTrain(treinNaam, componentNaam);
-		return tr.removeRollingComponent(component) && losseComponenten.add(component);
+		Trein tr = getTrein(treinNaam);																					// trein ophalen
+		RollingComponent component = getComponentFromTrain(treinNaam, componentNaam);									// wagon ophalen
+		if(tr != null && component != null) {																			// testen of trein en wagon bestaan
+			return tr.removeRollingComponent(component) && losseComponenten.add(component);								// wagon van trein afhalen en aan losseComponenten toevoegen
+		}
+		return false;
 	}
 
 	public int getNumWagonSeats(String name) {
@@ -72,12 +84,15 @@ public class TreinController implements TreinService {
 	}
 
 	public boolean createRollingComponent(String name, int gewicht, String typeNaam, int specialeWaarde) {
+		if(getComponent(name) != null) {																				// testen of naam al bestaat
+			return false;
+		}
 		ComponentType type = getComponentType(typeNaam, specialeWaarde);
 		RollingComponent wagon = new RollingComponent(name, type, gewicht);
 		return losseComponenten.add(wagon);
 	}
 	
-	public boolean createRollingComponent(String name, String typeNaam, int specialeWaarde) {
+	public boolean createRollingComponent(String name, String typeNaam, int specialeWaarde) {							// hier kan een builder voor gemaakt worden, maar nu geen tijd voor
 		return createRollingComponent(name, 100, typeNaam, specialeWaarde);
 	}
 
@@ -90,14 +105,14 @@ public class TreinController implements TreinService {
 	}
 
 	public RollingComponent getComponent(String componentNaam) {
-		RollingComponent wagon = null;
-		for(Trein tr : treinen) {
-			wagon = tr.getRollingComponent(componentNaam);
-			if(wagon != null) {return wagon;}
+		RollingComponent wagon = null;																					// wagon standaard null
+		for(Trein tr : treinen) {																						// loop over alle treinen
+			wagon = tr.getRollingComponent(componentNaam);																// probeer compoennt component op te vragen
+			if(wagon != null) {return wagon;}																			// geef terug zodra succesvol
 		}
 
-		for(RollingComponent comp : losseComponenten) {
-			if(comp.getName().equals(componentNaam)) {
+		for(RollingComponent comp : losseComponenten) {																	// als wagon nog steeds null is, loop over losseComponenten
+			if(comp.getName().equals(componentNaam)) {																	// als naam gelijk is, return de wagon
 				return comp;
 			}
 		}
@@ -105,16 +120,25 @@ public class TreinController implements TreinService {
 	}
 
 	public List<RollingComponent> getAlleComponenten() {
-		return Collections.unmodifiableList(losseComponenten);
+		return Collections.unmodifiableList(losseComponenten);															// geef een lijst terug die je niet aan kan passen
 	}
 
 	public boolean removeComponent(String componentNaam) {
-		RollingComponent wagon = getComponent(componentNaam);
-		return treinen.remove(wagon) || losseComponenten.remove(wagon); //TODO: loop treinen en verwijder de wagon i.p.v. wagon uit treinen lijst verwijderen
+		RollingComponent wagon = getComponent(componentNaam);															// component ophalen
+		if(losseComponenten.remove(wagon)) {																			// als component in losseComponenten zat
+			return true;
+		} else {
+			for(Trein tr : treinen) {																					// loop over treinen en verwijder de wagon
+				if(tr.removeRollingComponent(wagon)) {																	// stop zodra succesvol
+					return true;
+				}
+			}
+		}
+		return false;
 	}
 
 	private ComponentType getComponentType(String typeNaam, int specialeWaarde) {
-		ComponentTypeFactory factory = new TypeBasedComponentTypeFactory(typeNaam);
+		ComponentTypeFactory factory = new TypeBasedComponentTypeFactory(typeNaam);										// FACTORY METHOD!!!
 		return factory.createComponentType(specialeWaarde);
 	}
 }
