ff0cbe355dcf551ed88502f58531370e45bd0f9e
Frank van Kan <Frank.vanKan@student.hu.nl>
2018-12-16 20:40:00 +0100

    Finalised main

--- src/main/java/richrail/service/Main.java
+++ src/main/java/richrail/service/Main.java
@@ -1,65 +1,32 @@
 package richrail.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import richrail.logging.Logger;
 import richrail.persistence.DataSource;
 import richrail.persistence.JsonFileDataSource;
-import richrail.ui.*;
 import richrail.ui.java.DrawingService;
 import richrail.ui.java.MainWindow;
-import richrail.ui.java.TrainRegisterService;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-		TreinService service = new TreinController();
+	    TreinService service = new TreinController();
 
 		DataSource persistentie = new JsonFileDataSource(service);
-		//service.subscribeToChanges( (TreinEventListener) persistentie );
+        Logger logger = new Logger();
 
 		DrawingService drawingService = new DrawingService(service);
-		TrainRegisterService registerService = new TrainRegisterService(service, drawingService);
 
-		MainWindow window = new MainWindow(drawingService, registerService);
+		// Open window and subscribe it to changes
+		MainWindow window = new MainWindow(drawingService, service);
+		service.subscribeToChanges(window);
 
+		// Load trains from file
+		persistentie.loadTreinen();
 
-		//
-//		service.newTrein("tr1");
-//		//
-//		service.createRollingComponent("wg1", 10000, "vrachtcomponent", 300);
-//		service.createRollingComponent("wg2", 10000, "passagiercomponent", 300);
-//		service.addComponentToTrain("tr1", "wg1");
-//		service.addComponentToTrain("tr1", "wg2");
-//
-//
-//		service.createRollingComponent("wf1", 10000, "vrachtcomponent", 300);
-//		service.createRollingComponent("wf2", 10000, "passagiercomponent", 300);
-//
-//		service.newTrein("tr2");
-//		service.addComponentToTrain("tr2", "wf1");
-//		service.addComponentToTrain("tr2", "wf2");
-//
-//		ObjectMapper mapper = new ObjectMapper();
-//		try {
-//			System.out.println(mapper.writeValueAsString(service.getAlleTreinen()));
-//
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		//
-
-
-
-
-		//	service.addComponentToTrain("tr1", service.createRollingComponent("wg3", "passagiercomponent", 30));
-		//	service.addComponentToTrain("tr1", service.createRollingComponent("wg4", "passagiercomponent", 30));
-		//	service.addComponentToTrain("tr1", service.createRollingComponent("wg5", "passagiercomponent", 30));
-		//	service.addComponentToTrain("tr1", service.createRollingComponent("wg6", "vrachtcomponent", 3000));
-		//	service.addComponentToTrain("tr1", service.createRollingComponent("wg7", "vrachtcomponent", 3000));
-		//
-		//	System.out.println(service.getTrein("tr1").toString());
-
-		//	DSLMain.dsl();
+		// Subscribe IO to changes
+		service.subscribeToChanges( (TreinEventListener) persistentie );
+		service.subscribeToChanges(logger);
 
 	}
 
